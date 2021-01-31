987b0597e6b39744f4adb5fd1c6ad21fb3cb91ab
Will O'Beirne <wbobeirne@gmail.com>
2021-01-31 14:53:50 -0600

    Define a custom theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,29 @@
 import { createMuiTheme } from "@material-ui/core";
 
+const PTMono = {
+  fontFamily: "PTMono",
+  fontStyle: "normal",
+  fontDisplay: "swap",
+  fontWeight: 400,
+  src: `
+    local('PTMono'),
+    local('PTMono-Regular'),
+    url('/fonts/PTMono-Regular.ttf') format('ttf')
+  `,
+} as const;
+
 export const theme = createMuiTheme({
   palette: {
-    type: 'dark',
+    type: "dark",
+  },
+  typography: {
+    fontFamily: "PTMono, monospace",
+  },
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        "@font-face": [PTMono],
+      },
+    },
   },
 });
